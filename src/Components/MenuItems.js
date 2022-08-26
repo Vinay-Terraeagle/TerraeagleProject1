0084f4ef49bdc0f7e811d199aa38dba55f8a142d
Vinay-TE-004 <kulkarnivinay696@gmail.com>
2022-08-26 14:50:33 +0530

    main

--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -1,38 +1,39 @@
-export const Menulists = [
+export const MenuItems = [
     {
         title: 'Dashboard',
         path:'/Dashboard',
         Cname:'menuDropdown'
         // icnpath:
-    },
-    {
+      },
+      {
         title: 'MyPlan',
         path:'/MyPlan',
         Cname:'menuDropdown'
         // icnpath:
-    },
-    {
+      },
+      {
         title: 'Resources',
         path:'/Dashboard',
         Cname:'menuDropdown'
         // icnpath:
-    },
-    {
+      },
+      {
         title: 'Discover',
         path:'/Dashboard',
         Cname:'menuDropdown'
         // icnpath:
-    },
-    {
+      },
+      {
         title: 'Spaces',
         path:'/Dashboard',
         Cname:'menuDropdown'
         // icnpath:
-    },
-    {
+      },
+      {
         title: 'Settings',
         path:'/Dashboard',
         Cname:'menuDropdown'
         // icnpath:
-    },
-]
+      },
+];
+
