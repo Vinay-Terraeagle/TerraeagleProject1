4e67ef826620cce2dcd19b0a50ad0cfa0c875964
Vinay-TE-004 <111457332+Vinay-TE-004@users.noreply.github.com>
2022-08-29 10:31:10 +0530

    my progress UI design page

---
+++ src/Components/myProgressComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function myProgressComponent() {
+  return (
+    <React.Fragment>
+        <div className='container myprogress-container'>
+            
+
+        </div>
+    </React.Fragment>
+  )
+}
